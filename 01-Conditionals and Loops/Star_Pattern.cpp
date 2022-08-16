0df58f4845668163b8bdb005587c179fabcb3627
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-17 00:49:06 +0530

    Star pattern

---
+++ 01-Conditionals and Loops/Star_Pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   int i, space, rows, k = 0;
+    
+   cin >> rows;
+   for (i = 1; i <= rows; ++i, k = 0) {
+      for (space = 1; space <= rows - i; ++space) {
+         cout << " ";
+      }
+      while (k != 2 * i - 1) {
+         cout << "*";
+         ++k;
+      }
+      cout << endl;
+   }
+}
