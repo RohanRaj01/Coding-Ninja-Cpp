afcba5eba4f0b0b41bc16ffafcacb56b114c2687
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-19 11:12:06 +0530

    Arrange Numbers in Array
    You have been given an empty array(ARR) and its size N. The only input taken from the user will be N and you need not worry about the array.
    Your task is to populate the array using the integer values in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2

---
+++ 07-Arrays/Arrange_Numbers_in_Array.cpp
@@ -0,0 +1,53 @@
+/*
+// main code
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+		int *arr = new int[n];
+		arrange(arr, n);
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		delete [] arr;
+	}
+	
+}
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+  
+  
+void arrange(int *arr, int n)
+{
+
+    int val=1;
+    int start = 0 ,end = n-1;
+    while(start<=end){
+        if(val%2==1){
+    		arr[start]=val;
+    		val++;
+    		start++;
+        }  
+        else{
+            arr[end]=val;
+    		val++;
+    		end--;
+        }
+    }
+}
