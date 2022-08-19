8980c011e429d31bec383942b4049faec068ac17
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-19 11:06:40 +0530

    Linear Search

---
+++ 07-Arrays/Linear_Search.cpp
@@ -0,0 +1,41 @@
+// Main code
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int *arr = new int[n];
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> arr[i];
+		}
+		int val;
+		cin >> val;
+		cout << linearSearch(arr, n, val) << endl;
+	}
+	return 0;
+}
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  
+  
+int linearSearch(int *arr, int n, int x){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == x){
+        	return i;
+        }
+    }
+    return -1;
+}
