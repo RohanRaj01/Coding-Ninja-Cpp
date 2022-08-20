1d99be170b43731e81a281278f483acb49feadc9
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-20 21:42:11 +0530

    Code Insertion Sort
    Provided with a random integer array/list(ARR) of size N, you have been required to sort this array using 'Insertion Sort'.

---
+++ 08-Searching and Sorting/Code_Insertion_Sort.cpp
@@ -0,0 +1,61 @@
+/*
+
+// main code
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		insertionSort(input, size);
+
+		for (int i = 0; i < size; i++)
+		{
+			cout << input[i] << " ";
+		}
+
+		delete[] input;
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+____________________________________________________________________________________________________________________________________________________________________
+____________________________________________________________________________________________________________________________________________________________________
+*/
+
+
+void insertionSort(int *input, int size){
+     for(int i = 1; i < size; i++){
+         int current = input[i];
+         int j;
+         for(j = i - 1; j >= 0; j--){
+             if (current < input[j]){
+                 input[j + 1] = input[j];
+             }
+             else{
+                 break;
+             }
+         }
+         input[j + 1] = current;
+     }
+
+}
