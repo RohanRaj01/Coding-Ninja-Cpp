3ed7f9525d72b18662b6b301078e987b2ce98563
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-20 21:32:08 +0530

    Code Binary Search
    You have been given a sorted(in ascending order) integer array/list(ARR) of size N and an element X. Write a function to search this element in the given input array/list using 'Binary Search'. Return the index of the element in the input array/list. In case the element is not present in the array/list, then return -1.

---
+++ 08-Searching and Sorting/Code_Binary_Search.cpp
@@ -0,0 +1,61 @@
+/*
+
+// main code
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+
+	int size;
+	cin >> size;
+	int *input = new int[size];
+
+	for(int i = 0; i < size; ++i)
+	{
+		cin >> input[i];
+	}
+	
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int val;
+		cin >> val;
+		cout << binarySearch(input, size, val) << endl;
+	}
+	
+	delete [] input;
+	return 0;
+}
+
+
+
+______________________________________________________________________________________________________________________________________________________________________
+______________________________________________________________________________________________________________________________________________________________________
+*/
+
+
+
+int binarySearch(int *input, int n, int val){
+    
+    int start = 0, end = n - 1;
+    while (start <= end){
+        int mid = (start + end) / 2;
+        if (input[mid] == val){
+            return mid;
+        }
+        else if(val < input[mid]){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
