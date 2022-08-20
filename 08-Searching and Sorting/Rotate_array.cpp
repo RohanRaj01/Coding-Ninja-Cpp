9d0972130757e06e398af37bb942e890a2927e97
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-08-20 21:52:11 +0530

    Rotate array by n number of element of array
    You have been given a random integer array/list(ARR) of size N. Write a function that rotates the given array/list by D elements(towards the left)

--- 08-Searching and Sorting/Rotate_array.cpp
+++ 08-Searching and Sorting/Rotate_array.cpp
@@ -1,1 +1,69 @@
+/*
 
+// main code
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size;
+		cin >> size;
+
+		int *input = new int[size];
+
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+
+		int d;
+		cin >> d;
+
+		rotate(input, d, size);
+
+		for (int i = 0; i < size; ++i)
+		{
+			cout << input[i] << " ";
+		}
+		
+		delete[] input;
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+_________________________________________________________________________________________________________________________________________________________________
+_________________________________________________________________________________________________________________________________________________________________
+*/
+
+
+void rotate(int *input, int d, int n){
+    
+    int temp[d];
+	for (int i = 0; i < d; i++){
+    	temp[i] = input[i];
+    }
+      
+    int x = 0;
+  	for(int j = d; j < n; j++){
+    	input[x] = input[j];
+    	x++;
+  	}
+  
+  	x = 0;
+  	for (int k = n - d; k < n; k++){
+    	input[k] = temp[x];
+    	x++;
+  	}
+    
+}
